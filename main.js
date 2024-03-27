f59a319ad7362ca2619688a9eb66af9f1f46bdbf
andiinurfitriana <andiinurfitriana@apps.ipb.ac.id>
2024-03-27 23:14:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function toggleDetail(e) {
+    const target = $(e.target)
+
+    if ($(target).hasClass("active")) {
+        $(target).html("More Info").removeClass("active")
+    } else {
+        $(target).html("Less Info").addClass("active")
+    }
+
+    const item = $(target).parents(".about-exp-item")
+    const detail = $(item).children(".about-exp-item-detail")
+    $(detail).slideToggle()
+}
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if (!$(email).val()) {
+        alert("Email is required")
+    } else if (!$(subject).val()) {
+        alert("Subject is required")
+    } else if (!$(message).val()) {
+        alert("Message is required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
+
+$(document).on("click", ".work-img-item", function () {
+    let newPageURL = $(this).data('link');
+    // Open the link in a new tab
+    window.open(newPageURL, "_blank");
+});
